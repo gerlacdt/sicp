d81bed434a645e4df4b82a4bfe70c70d8e59f9b7
Daniel Gerlach <wrack1981@gmx.de>
2014-05-11 20:18:41 +0200

    ackermann function, nest function into functions

--- ch1.rkt
+++ ch1.rkt
@@ -1,4 +1,6 @@
 (module ch1 racket
+  (require racket/trace)
+  
   (define (fib n)
     (define (fib-iter a b n)
       (if (= n 0)
@@ -18,39 +20,72 @@
           ((and (> y z) (> y x) (> z x)) (sum-of-squares y z))
           (else (sum-of-squares x z))))
 
-  (define (sqrt-iter guess x)
-    (if (good-enough? guess (improve guess x))
-        guess
-        (sqrt-iter (improve guess x) x)))
-
-  (define (improve guess x)
-    (average guess (/ x guess)))
-
-  (define (average x y)
-    (/ (+ x y) 2))
-
-  (define (good-enough? guess new-guess)
-    (< (abs (- guess new-guess)) 0.001))
-
   (define (sqrt x)
-    (sqrt-iter 1.0 x))
+    (define (sqrt-iter guess)
+      (if (good-enough? guess (improve guess))
+          guess
+          (sqrt-iter (improve guess))))
+    (define (improve guess)
+      (average guess (/ x guess)))
+    (define (average x y)
+      (/ (+ x y) 2))
+    (define (good-enough? guess new-guess)
+      (< (abs (- guess new-guess)) 0.001))
+    (sqrt-iter 1.0))
 
   (define (cube x)
     (* x x x))
 
-  (define (cube-root-iter guess x)
-    (if (good-enough-cube? guess (improve-cube guess x))
-        guess
-        (cube-root-iter (improve-cube guess x) x)))
-
-  (define (improve-cube guess x)
-    (/ (+ (* 2 guess) (/ x (* guess guess))) 3))
-
-  (define (good-enough-cube? guess new-guess)
-    (< (abs (- guess new-guess)) 0.00001))
-
   (define (cube-root x)
-    (cube-root-iter 1.0 x))
+    (define (cube-root-iter guess)
+      (if (good-enough? guess (improve guess))
+          guess
+          (cube-root-iter (improve guess))))
+    (define (improve guess)
+      (/ (+ (* 2 guess) (/ x (* guess guess))) 3))
+    (define (good-enough? guess new-guess)
+      (< (abs (- guess new-guess)) 0.00001))
+    (cube-root-iter 1.0))
+
+  (define (factorial n)
+    (cond ((= n 1) 1)
+          (else (* n (factorial (- n 1))))))
+
+  (define (fact-iter n)
+    (define (fact-helper product counter max-count)
+      (if (> counter max-count)
+          product
+          (fact-helper (* product counter) (+ counter 1) max-count)))
+    (fact-helper 1 1 n))
+
+  (define (plus a b)
+    (if (= a 0)
+        b
+        (inc (plus (dec a) b))))
+
+  (define (plus2 a b)
+    (if (= a 0)
+        b
+        (plus2 (dec a) (inc b))))
+
+  (define (inc n)
+    (+ n 1))
+  
+  (define (dec n)
+    (- n 1))
+
+  (define (A x y)
+    ;; Ackerman function
+    (cond ((= y 0) 0)
+          ((= x 0) (* 2 y))
+          ((= y 1) 2)
+          (else (A (- x 1) (A x (- y 1))))))
+
+  (define (pow-2 n)
+    (A 1 n))
+
+  (define (double n)
+    (A 0 n))
 
   (define (done)
     'mydone1)
