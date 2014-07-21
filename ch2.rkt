664bd01da7bc7af06350f7256699c837f0bc1799
Daniel Gerlach <wrack1981@gmx.de>
2014-07-21 22:04:09 +0200

    added binary trees and some unit tests

--- ch2.rkt
+++ ch2.rkt
@@ -565,6 +565,38 @@
 
 (union-set-ordered '(1 2 3 4 5) '(2 3 4 5 6))
 
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-treeset? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-treeset? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-treeset? x (right-branch set)))))
+
+(define (adjoin-treeset x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set) 
+                    (adjoin-treeset x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-treeset x (right-branch set))))))
+
 (define (new-method) 
   'new-method)
 
--- test/ch2_test.rkt
+++ test/ch2_test.rkt
@@ -12,7 +12,16 @@
 
    (test-case
     "test horner formula"
-    (check-equal? 79 (horner-eval 2 (list 1 3 0 5 0 1))))))
+    (check-equal? 79 (horner-eval 2 (list 1 3 0 5 0 1))))
+
+   (test-case
+    "test binary tree implentation")
+   (let* ((tree (make-tree 5 (make-tree 3 (make-tree 1 null null) (make-tree 4 null null)) (make-tree 7 null null))))
+     (check-equal? false (element-of-treeset? 2 tree))
+     (check-equal? true (element-of-treeset? 5 tree))
+     (check-equal? true (element-of-treeset? 7 tree))
+     (check-equal? true (element-of-treeset? 1 tree))
+     (check-equal? true (element-of-treeset? 4 tree)))))
 
 (run-tests sicp-ch2-tests)
 
