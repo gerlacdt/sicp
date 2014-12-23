1fc84e9e549f4669ed8bf091c15f8ef462154bc2
Daniel Gerlach <wrack1981@gmx.de>
2014-12-23 18:16:32 +0100

    added generic arithmetic packages

--- ch2_complex.rkt
+++ ch2_complex.rkt
@@ -96,62 +96,194 @@
   (put 'magnitude '(polar) magnitude)
   (put 'angle '(polar) angle)
   (put 'make-from-real-imag 'polar
+        (lambda (x y) (tag (make-from-real-imag x y))))
+   (put 'make-from-mag-ang 'polar
+        (lambda (r a) (tag (make-from-mag-ang r a))))
+   '(DONE INITIALIZED POLAR PACKAGE))
+
+ (install-rectangular-package)
+ (install-polar-package)
+
+ (define (apply-generic op . args)
+   (let* ((type-tags (map type-tag args))
+          (proc (get op type-tags)))
+     (displayln (format "type tags: ~a" type-tags))
+     (if proc
+         (apply proc (map contents args))
+         (error "No method for these types -- APPLY GENERIC"
+                (list op type-tags)))))
+
+ (define (real-part z)
+   (apply-generic 'real-part z))
+
+ (define (imag-part z)
+   (apply-generic 'imag-part z))
+
+ (define (magnitude z)
+   (apply-generic 'magnitude z))
+
+ (define (angle z)
+   (apply-generic 'angle z))
+
+ (define (make-from-real-imag x y)
+   ((get 'make-from-real-imag 'rectangular) x y))
+
+ (define (make-from-mag-ang r a)
+   ((get 'make-from-mag-ang 'polar) r a))
+
+ ;;; message passing
+
+ ;;; commented out because same methods exist already
+
+ ;; (define (make-from-real-imag x y)
+ ;;   (define (dispatch op)
+ ;;     (cond ((eq? op 'real-part) x)
+ ;;           ((eq? op 'imag-part) y)
+ ;;           ((eq? op 'magnitude)
+ ;;            (sqrt (+ (square x) (square y))))
+ ;;           ((eq? op 'angle) (atan y x))
+ ;;           (else (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+ ;;   dispatch)
+
+ ;; (define (apply-generic op arg)
+ ;;   (arg op))
+
+ ;; ;;; ex 2.75
+ ;; (define (make-from-mag-ang r a)
+ ;;   (define (dispatch op)
+ ;;     (cond ((eq? op 'real-part) (* r (cos a)))
+ ;;           ((eq? op 'imag-part) (* r (sin a)))
+ ;;           ((eq? op 'magnitude) r)
+ ;;           ((eq? op 'angle) a)
+ ;;           (else (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+ ;;   dispatch)
+
+
+ ;;; generic arithmetic operations
+
+ (define (add x y)
+   (apply-generic 'add x y))
+
+ (define (sub x y)
+   (apply-generic 'sub x y))
+
+ (define (mul x y)
+   (apply-generic 'mul x y))
+
+ (define (div x y)
+   (apply-generic 'div x y))
+
+
+ (define (install-scheme-number-package)
+   (define (tag x)
+     (attach-tag 'scheme-number x))
+   (put 'add '(scheme-number scheme-number)
+        (lambda (x y) (tag (+ x y))))
+   (put 'sub '(scheme-number scheme-number)
+        (lambda (x y) (tag (- x y))))
+   (put 'mul '(scheme-number scheme-number)
+        (lambda (x y) (tag (* x y))))
+   (put 'div '(scheme-number scheme-number)
+        (lambda (x y) (tag (/ x y))))
+   (put 'make 'scheme-number
+        (lambda (x) (tag x)))
+   '(DONE INITIALIZED SCHEME NUMBER PACKAGE))
+
+ (install-scheme-number-package)
+
+ (define (make-scheme-number n)
+   ((get 'make 'scheme-number) n))
+
+
+ (define (install-rational-package)
+   ;; internal procedures
+   (define (numer x) (car x))
+   (define (denom x) (cdr x))
+   (define (make-rat n d)
+     (let ((g (my-gcd n d)))
+       (cons (/ n g) (/ d g))))
+   (define (add-rat x y)
+     (make-rat (+ (* (numer x (denom y)))
+                  (* (numer y) (denom x)))
+               (* (denom x) (denom y))))
+   (define (sub-rat x y)
+     (make-rat (- (* (numer x) (denom y))
+                  (* (numer y) (denom x)))
+               (* (denom x) (denom y))))
+   (define (mul-rat x y)
+     (make-rat (* (numer x) (numer y))
+               (* (denom x) (denom y))))
+   (define (div-rat x y)
+     (make-rat (* (numer x) (denom y))
+               (* (denom x) (numer y))))
+   ;; interface to the rest of the system
+   (define (tag x) (attach-tag 'rational x))
+   (put 'add '(rational rational)
+        (lambda (x y) (tag (add-rat x y))))
+   (put 'sub '(rational rational)
+        (lambda (x y) (tag (sub-rat x y))))
+   (put 'mul '(rational rational)
+        (lambda (x y) (tag (mul-rat x y))))
+   (put 'div '(rational rational)
+        (lambda (x y) (tag (div-rat x y))))
+
+   (put 'make 'rational
+        (lambda (n d) (tag (make-rat n d))))
+   '(DONE INITIALIZED RATIONAL PACKAGE))
+
+ (install-rational-package)
+
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular and polar packages
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z2) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  ;; interface to the rest of the system
+  (define (tag z)
+    (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
        (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'polar
+  (put 'make-from-mag-ang 'complex
        (lambda (r a) (tag (make-from-mag-ang r a))))
-  '(DONE INITIALIZED POLAR PACKAGE))
 
-(install-rectangular-package)
-(install-polar-package)
-
-(define (apply-generic op . args)
-  (let* ((type-tags (map type-tag args))
-         (proc (get op type-tags)))
-    (if proc
-        (apply proc (map contents args))
-        (error "No method for these types -- APPLY GENERIC"
-               (list op type-tags)))))
-
-(define (real-part z)
-  (apply-generic 'real-part z))
-
-(define (imag-part z)
-  (apply-generic 'imag-part z))
-
-(define (magnitude z)
-  (apply-generic 'magnitude z))
-
-(define (angle z)
-  (apply-generic 'angle z))
-
-(define (make-from-real-imag x y)
-  ((get 'make-from-real-imag 'rectangular) x y))
-
-(define (make-from-mag-ang r a)
-  ((get 'make-from-mag-ang 'polar) r a))
-
-;;; higher functions
-(define (add-complex z1 z2)
-  (make-from-real-imag (+ (real-part z1) (real-part z2))
-                       (+ (imag-part z1) (imag-part z2))))
-
-(define (sub-complex z1 z2)
-  (make-from-real-imag (- (real-part z1) (real-part z2))
-                       (- (imag-part z1) (imag-part z2))))
-
-(define (mul-complex z1 z2)
-  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
-                     (+ (angle z1) (angle z2))))
-
-(define (div-complex z1 z2)
-  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
-                     (- (angle z1) (angle z2))))
-
-(define c1 (make-from-real-imag 2 4))
-(define c2 (make-from-real-imag 3 9))
-
-(define c3 (make-from-mag-ang 3 9))
-(define c4 (make-from-mag-ang 2 4))
+  ;; ex 2.77
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  '(DONE INITIALIZED COMPLEX PACKAGE))
 
 
-;;; message passing
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+(install-complex-package);
