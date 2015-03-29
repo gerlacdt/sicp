508427bc50decb3f2afbcf90bfab23163e200471
Daniel Gerlach <wrack1981@gmx.de>
2015-03-29 19:43:09 +0200

    added memoize function with memo-fib

--- ch3.rkt
+++ ch3.rkt
@@ -210,6 +210,22 @@
              (error "Unknown request -- MAKE-QUEUE-DISPATCH"))))
     dispatch))
 
+;; ex. 3.27
+
+(define (memoize f)
+  (let ((table (make-hash)))
+    (lambda (x)
+      (let ((previously-computed-result (hash-ref table x #f)))
+        (or previously-computed-result
+            (let ((result  (f x)))
+              (hash-set! table x result)
+              result))))))
+
+(define memo-fib (memoize (lambda (n)
+                            (cond ((= n 0) 0)
+                                  ((= n 1) 1)
+                                  (else (+ (memo-fib (- n 1))
+                                           (memo-fib (- n 2))))))))
 
 ;; concurrency
 (define (parallel-execute . procs)
